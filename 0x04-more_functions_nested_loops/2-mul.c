b6159a21b368b0129354ce2ffc556bc502cc1fc6
Amanuel-Nig <amanuelnigussieedu@gmail.com>
2022-03-17 23:13:37 +0530

    c programming

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - multiply two numbers
+ * @a - int to be multiplied
+ * @b - int to be multiplied
+ * Return: product of the two numbers
+ */
+int mul(int a, int b)
+{
+    int product;
+
+    product = a * b ;
+    return (product);
+}
