ad34eea3e30e815770382d806e956caef421184c
Amanuel-Nig <amanuelnigussieedu@gmail.com>
2022-03-17 22:59:31 +0530

    c programming

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,13 @@
 /**
- * _isdigit - checks if it is digit
- * 
- * @c: character to check
+ * _isdigit - check if a character is a digit
+ *
+ * @c: character to determine
  *
  * Return: gives 1 or 0
  */
 int _isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
-	{
 		return (1);
-	}
 	return (0);
 }
