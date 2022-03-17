bc27824fcc938e2bce6c9ab23a3037efc0616039
Amanuel-Nig <amanuelnigussieedu@gmail.com>
2022-03-17 22:51:17 +0530

    c programming

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if it is digit
+ * @c: character to check
+ *
+ * Return: gives 1 or 0
+ */
+int _isupper(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
