86c676f0b41f4af23037d1d9f9e5781bb7f924be
Amanuel-Nig <amanuelnigussieedu@gmail.com>
2022-04-02 00:49:47 +0530

    c programming

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,20 +3,20 @@
 #include<ctype.h>
 
 /**
- * main - add postitve numbers
+ * main - add postitve numbers.
  * @argc: counts the arguments passed to the function.
  * @argv: gives the value for the argument passed to the function.
  *
  * Return: Returns always success.
  */
+
 int main(int argc, char *argv[])
 {
-int i, sum = 0;
-if (argc == 1)
-{
-printf("%d\n", 0);
-}
-else (argc > 1)
+int i;
+int sum;
+
+sum = 0;
+if (argc > 1)
 {
 for (i=1; i < argc; i++)
 {
@@ -33,4 +33,8 @@ printf("%c\n", "Error");
 return (1);
 }
 }
+else (argc == 1)
+{
+printf("%s\n", "Error");
+return (1);
 }
