bd18ce52fffd114cfa195f0db029a552add8000f
Amanuel-Nig <amanuelnigussieedu@gmail.com>
2022-04-01 21:20:59 +0530

    c programming

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - is a funtion that prints the number of arguments passed into it.
+ *@argc: is an argument counter for char.
+ *@argv: is an argument value for char.
+ *Return: Returns always success.
+**/
+
+int main(int argc, char *argv[])
+{
+	printf("args = %d\n", argc);
+	return(0);
+}
