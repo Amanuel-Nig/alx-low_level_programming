5bd98d2782d95e2be530c00f4cd205a781ec7027
Amanuel-Nig <amanuelnigussieedu@gmail.com>
2022-04-13 22:34:27 +0530

    c programming

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,50 @@
+/**
+ * op_add - adds two integers.
+ * @a: integer 1.
+ * @b: integer 2.
+ * Return: returns summation.
+ **/
+int op_add(int a, int b)
+{
+return(a + b);
+}
+/**
+ * op_sub - subtracts two integers.
+ * @a: integer 1.
+ * @b: integer 2.
+ * Return: returns difference.
+ **/
+int op_sub(int a, int b);
+{
+return(a - b);
+}
+/**
+ * op_mul - multiplies two integers.
+ * @a: integer 1.
+ * @b: integer 2.
+ * Return: returns product.
+ **/
+int op_mul(int a, int b)
+{
+return(a * b);
+}
+/**
+ * op_div - divides two integers.
+ * @a: integer 1.
+ * @b: integer 2.
+ * Return: returns qoutient.
+ **/
+int op_div(int a, int b)
+{
+return(a / b);
+}
+/**
+ * op_mod - modules two numbers.
+ * @a: integer 1.
+ * @b: integer 2.
+ * Return: returns remainder.
+ **/
+int op_mod(int a, int b)
+{
+return(a % b);
+}
